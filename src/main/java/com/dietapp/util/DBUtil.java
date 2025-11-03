7231e4eecb8a489bdad76cbfe1077b770b17e922
omshukla1712 <omshukla@gmail.com>
2025-11-03 11:36:06 +0530

    Updated DBUtil to use Railway environment variables

--- src/main/java/com/dietapp/util/DBUtil.java
+++ src/main/java/com/dietapp/util/DBUtil.java
@@ -1,10 +1,12 @@
 package com.dietapp.util;
+
 import java.sql.*;
 
 public class DBUtil {
-    private static final String URL = "jdbc:postgresql://localhost:5432/dietdb";
-    private static final String USER = "dietuser";
-    private static final String PASSWORD = "dietpass";
+    private static final String URL = System.getenv("DB_URL");
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASSWORD = System.getenv("DB_PASS");
+
     static {
         try {
             Class.forName("org.postgresql.Driver");
